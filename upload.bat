27a5801b14902196ee8adaad303303a00ea3db57
QuantumNecro <QuantumNecro@gmail.com>
2022-12-27 00:20:06 +0800

    upload

---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.name "QuantumNecro"
+git config --local user.email "QuantumNecro@gmail.com"
+git remote set-url origin git@AAL:QuantumNecro/Tools.git
+
+git pull
+git add .
+git commit -m "upload"
+git push
