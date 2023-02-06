86516daf8e1357763a0c59e3ac051b952b7e5be3
QuantumNecro <56330595+QuantumNecro@users.noreply.github.com>
2023-02-06 11:51:37 +0800

    Update convert.ps1

--- Whisper/convert.ps1
+++ Whisper/convert.ps1
@@ -1,4 +1,3 @@
 <<WhisperInstallPath>>\Whisper\Scripts\Activate.ps1
 ffmpeg -i input.mkv -vn -acodec copy output.m4a
-whisper output.m4a --language zh --model medium > output.txt
 python ./conpert.py
