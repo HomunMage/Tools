98616f34d2f74c3876a2700a64388e0803ae387a
LatticeMage <lattice@posetmage.com>
2024-09-29 14:05:59 +0800

    move to repo

--- OS/Docker/RemoteUbuntu/start.sh
+++
@@ -1,4 +0,0 @@
-docker build -t ubuntu-ssh --build-arg SSH_USERNAME=<name> --build-arg SSH_PASSWORD=<password> .
-docker create -it -p 5566:22 -v "$(pwd)":<mnt_source_path> -w /home/<name>/app --name ubuntu-container ubuntu-ssh /bin/bash
-docker start ubuntu-container
-ssh -i /path/to/your/private_key -p 5566 <name>@127.0.0.1
--- OS/Docker/index.md
+++
@@ -1,3 +0,0 @@
----
-layout: page/note/directory
----
